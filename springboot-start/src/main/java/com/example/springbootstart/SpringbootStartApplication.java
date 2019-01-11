f5275f65d1c7ac14fbe593380d8db76fce8ec74b
kinsonfu <2444179219@qq.com>
2019-01-11 22:57:55 +0800

    'init'

---
+++ springboot-start/src/main/java/com/example/springbootstart/SpringbootStartApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbootstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStartApplication.class, args);
+	}
+
+}
+
