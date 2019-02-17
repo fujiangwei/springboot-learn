d3cb9bf4ccc78249f24aed35797684bef94f7511
kinsonfu <2444179219@qq.com>
2019-02-17 21:43:06 +0800

    springboot定时任务

--- springboot-scheduled/src/main/java/com/example/springbootscheduled/SpringbootScheduledApplication.java
+++ springboot-scheduled/src/main/java/com/example/springbootscheduled/SpringbootScheduledApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 //开启对定时任务的支持，这种方式是在同一个线程中串行执行，适合单个线程任务处理
-//@EnableScheduling
+@EnableScheduling
 public class SpringbootScheduledApplication {
 
 	public static void main(String[] args) {
