9a1908c99184fad9463e817d5860ed86ce785c31
kinsonfu <2444179219@qq.com>
2019-08-25 23:20:08 +0800

    111111

--- springboot-rpc/src/test/java/com/example/springbootrpc/client/HelloService.java
+++
@@ -1,7 +0,0 @@
-package com.example.springbootrpc.client;
-
-public interface HelloService {
-    String hello(String name);
-
-    String hello(Person person);
-}
--- springboot-rpc/src/test/java/com/example/springbootrpc/client/PersonService.java
+++
@@ -1,7 +0,0 @@
-package com.example.springbootrpc.client;
-
-import java.util.List;
-
-public interface PersonService {
-    List<Person> GetTestPerson(String name, int num);
-}
